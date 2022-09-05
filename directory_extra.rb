1923b9b87d8a2259e49bb019f780ce75f0d81c96
kateusacova <kateusacova@gmail.com>
2022-09-05 10:02:18 +0100

    Exercise 6

--- directory_extra.rb
+++ directory_extra.rb
@@ -1,37 +1,34 @@
 def input_students
-  puts "Please enter the name, cohort and place of birth of the students"
+  puts "Please enter the name and cohort of the students"
   puts "To finish, just leave all fields blank"
 
   students =  []
-  # asking for more information
   name = gets.chomp
   cohort = gets.chomp.to_sym
-  country = gets.chomp
-
+  
   while !name.empty? do
-    students << {name: name, cohort: cohort, country: country}
+    students << {name: name, cohort: cohort}
     puts "Now we have #{students.count} students"
     name = gets.chomp
     cohort = gets.chomp.to_sym
-    country = gets.chomp
   end
 
   students
 end
-
+# using center() method to align the output
 def print_header
-  puts "The students of Villains Academy"
-  puts "-------------"
+  puts "The students of Villains Academy".center(50)
+  puts "-------------".center(50)
 end
 
 def print(students)
   students.each.with_index(1) { |student, i|
-    puts "#{i}. #{student[:name]} from #{student[:country]} (#{student[:cohort]} cohort)"
+    puts "#{i}. #{student[:name]} (#{student[:cohort]} cohort)".center(50)
   }
 end
 
 def print_footer(students)
-  puts "Overall, we have #{students.count} great students"
+  puts "Overall, we have #{students.count} great students".center(50)
 end
 
 students = input_students
